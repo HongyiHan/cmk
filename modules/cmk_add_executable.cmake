a716b6df7c7c48ae44dbee663b8dba00ca478082
HongyiHan <hongyi_han@163.com>
2022-06-01 14:23:17 +0800

    Add modules/cmk_add_executable.cmake

---
+++ modules/cmk_add_executable.cmake
@@ -0,0 +1,24 @@
+function(cmk_add_executable)
+
+    # cmk_add_executable(<target>
+    #                    <source>...
+    #                    [OUTPUT_NAME <name>]
+    #                    [LINK_LIBRARIES <library>...]
+    #                    [SOURCE_FILES <source>...])
+    set(ARG_TARGET ${ARGV0})
+    set(options)
+    set(oneValueArgs OUTPUT_NAME)
+    set(multiValueArgs LINK_LIBRARIES SOURCE_FILES)
+    cmake_parse_arguments(PARSE_ARGV 1 "ARG" "${options}" "${oneValueArgs}" "${multiValueArgs}")
+
+    add_executable(${ARG_TARGET} ${ARG_UNPARSED_ARGUMENTS} ${ARG_SOURCE_FILES})
+
+    if(ARG_OUTPUT_NAME)
+        set_property(TARGET ${ARG_TARGET} PROPERTY OUTPUT_NAME ${ARG_OUTPUT_NAME})
+    endif()
+
+    if(ARG_LINK_LIBRARIES)
+        target_link_libraries(${ARG_TARGET} ${ARG_LINK_LIBRARIES})
+    endif()
+
+endfunction()
