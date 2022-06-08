d8567b5cc2c845004e2e0323a85820eec98594b3
HongyiHan <hongyi_han@163.com>
2022-06-08 16:31:33 +0800

    Add output 'TARGET_NAME' to cmk_sc_add_rel_pth_exe()

--- modules/cmk_sc_add_rel_pth_exe.cmake
+++ modules/cmk_sc_add_rel_pth_exe.cmake
@@ -14,45 +14,50 @@ include(${CMAKE_CURRENT_LIST_DIR}/cmk_get_relative_path.cmake)
 #   b. 将相对路径中的 '/' 替换为 '__' 得到目标名称的前缀
 #   c. 最终的目标名称为：目标名称的前缀 + '__' + 调用者传入的名称
 # 2. 默认的源文件
-#   a. ${target}.c
-#   b. ${target}.cpp
+#   a. ${target_hint}.c
+#   b. ${target_hint}.cpp
 function(cmk_sc_add_rel_pth_exe)
 
-    # cmk_sc_add_rel_pth_exe(<target>
-    #                        <source>...
-    #                        [OUTPUT_NAME <name>]
-    #                        [LINK_LIBRARIES <library>...]
-    #                        [SOURCE_FILES <source>...])
-    set(ARG_TARGET ${ARGV0})
+    # cmk_sc_add_rel_pth_exe(<target_hint>                      input
+    #                        <source>...                        input
+    #                        [OUTPUT_NAME <name>]               input
+    #                        [LINK_LIBRARIES <library>...]      input
+    #                        [SOURCE_FILES <source>...]         input
+    #                        [TARGET_NAME <name>])              output
+    set(ARG_TARGET_HINT ${ARGV0})
     set(options)
-    set(oneValueArgs OUTPUT_NAME)
+    set(oneValueArgs OUTPUT_NAME TARGET_NAME)
     set(multiValueArgs LINK_LIBRARIES SOURCE_FILES)
     cmake_parse_arguments(PARSE_ARGV 1 "ARG" "${options}" "${oneValueArgs}" "${multiValueArgs}")
 
     cmk_get_relative_path(CURRENT_DIRECTORY_RELATIVE_PATH)
     string(REPLACE / __ TARGET_NAME_PREFIX ${CURRENT_DIRECTORY_RELATIVE_PATH})
-    set(TARGET_FULL_NAME ${TARGET_NAME_PREFIX}__${ARG_TARGET})
+    set(TARGET_NAME ${TARGET_NAME_PREFIX}__${ARG_TARGET_HINT})
 
     set(TARGET_OUTPUT_NAME ${ARG_OUTPUT_NAME})
     if(NOT TARGET_OUTPUT_NAME)
-        set(TARGET_OUTPUT_NAME ${ARG_TARGET})
+        set(TARGET_OUTPUT_NAME ${ARG_TARGET_HINT})
     endif()
 
     list(APPEND TARGET_SOURCE_FILES ${ARG_SOURCE_FILES} ${ARG_UNPARSED_ARGUMENTS})
     list(LENGTH TARGET_SOURCE_FILES TARGET_SOURCE_FILES_LENGTH)
     if(NOT TARGET_SOURCE_FILES_LENGTH)
-        if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_TARGET}.c)
-            list(APPEND TARGET_SOURCE_FILES ${ARG_TARGET}.c)
-        elseif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_TARGET}.cpp)
-            list(APPEND TARGET_SOURCE_FILES ${ARG_TARGET}.cpp)
+        if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_TARGET_HINT}.c)
+            list(APPEND TARGET_SOURCE_FILES ${ARG_TARGET_HINT}.c)
+        elseif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_TARGET_HINT}.cpp)
+            list(APPEND TARGET_SOURCE_FILES ${ARG_TARGET_HINT}.cpp)
         else()
-            message(FATAL_ERROR "Default source file not found for target ${ARG_TARGET}.")
+            message(FATAL_ERROR "Default source file not found for target_hint ${ARG_TARGET_HINT}.")
         endif()
     endif()
 
-    cmk_add_executable(${TARGET_FULL_NAME}
+    cmk_add_executable(${TARGET_NAME}
                        OUTPUT_NAME ${TARGET_OUTPUT_NAME}
                        SOURCE_FILES ${TARGET_SOURCE_FILES}
                        LINK_LIBRARIES ${ARG_LINK_LIBRARIES})
+
+    if(ARG_TARGET_NAME)
+        set(${ARG_TARGET_NAME} ${TARGET_NAME} PARENT_SCOPE)
+    endif()
 
 endfunction()
